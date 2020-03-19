40910184af744b691adc2be74bb7474ac00da6f5
MARTA RESZETOW <marta.reszetow@gmail.com>
2020-03-19 12:44:43 +0100

    added secutrity options for users requests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.Scanner;
 
@@ -18,7 +19,7 @@ public class Main {
             .build();
 
     public static void main(String[] args) {
-        // zapytanie do linku
+// _____________________________ generowanie linku w zaeżności od rodzaju tabeli _____________________________________________
         // "A" i "C"        http://api.nbp.pl/api/exchangerates/rates/{table}/{currency}/{endDate}/?format=json
         // "B"              http://api.nbp.pl/api/exchangerates/tables/B/{startDate}/{endDate}/?format=json
         String link = "http://api.nbp.pl/api/exchangerates";
@@ -31,11 +32,26 @@ public class Main {
         String endData;
         String format = "/?format=json";
 
-
+// ______ zapytania do użytkowinika o wprowadzenie danych i wstępna weryfikacja ich poprawności (rodzaj tabeli/waluta)__________
         System.out.println("Podaj rodzaj tabeli (A/B/C)");
         tableType = scan.nextLine().toUpperCase();
+        if (!tableType.equals("A") && !tableType.equals("B") && !tableType.equals("C")) {
+            System.err.println("Błędnie podany rodzaj tabeli. Spróbuj ponownie");
+            tableType = scan.nextLine().toUpperCase();
+        }
         System.out.println("Podaj walutę, dla której chcesz spradzić statystyki:");
         currencyChoice = scan.nextLine().toUpperCase();
+        try {
+            if (!(Currency.valueOf(currencyChoice)).toString().equals(currencyChoice)) {
+                // jeżeli nie ma takiej waluty w dostępnych opcjach w enumie Currency to obsluguje wyjatek i
+                // prosi użyytkownika o ponowne wpisanie waluty
+            }
+        } catch (IllegalArgumentException eeeexc) {
+            System.err.println("Błędnie podana waluta. Spróbuj ponownie.");
+            currencyChoice = scan.nextLine().toUpperCase();
+
+        }
+
         System.out.println("Podaj datę od której chcesz rozpocząć sprawdzanie kursów walut w formacie(RRRR-MM-DD): ");
         startData = scan.nextLine();
         System.out.println("Podaj datę od której chcesz rozpocząć sprawdzanie kursów walut w formacie(RRRR-MM-DD): ");
@@ -55,104 +71,109 @@ public class Main {
 
         try {
 
-            // zacięgnięcie odpowiedzi do zapytania
-            HttpResponse<String> resp = HTTP_CLIENT.send(request,
-                    HttpResponse.BodyHandlers.ofString());
+            try {
+//____________________________________ zacięgnięcie odpowiedzi do zapytania______________________________________________________
 
-            String responseBody = resp.body(); // cialo odpowiedzi - jest w formacie json
+                HttpResponse<String> resp = HTTP_CLIENT.send(request,
+                        HttpResponse.BodyHandlers.ofString());
 
-            // odczytanie tresci z jsona
+                String responseBody = resp.body(); // cialo odpowiedzi - jest w formacie json
 
-            ObjectMapper objectMapper = new ObjectMapper();
+//_________________________________________ odczytanie tresci z jsona ____________________________________________________________
 
-            // - dla tabel(A i C) wynikiem będzie tabela a gdzieś głębiej ratesy
-            // dla tabeli B od razu mamy liste
+                ObjectMapper objectMapper = new ObjectMapper();
 
-
-            TableTypeA importedDataA = null;
-            List<TableTypeB> importedDataB = null;
-            TableTypeC importedDataC = null;
+                // dla tabel(A i C) wynikiem będzie tabela a gdzieś głębiej ratesy
+                // dla tabeli B mamy listę
 
 
-            if (tableType.equalsIgnoreCase("A")) {
-                importedDataA = objectMapper.readValue(responseBody,
-                        TableTypeA.class);
-                //     String printRates = importedDataA.toString();
-                //     System.out.println(printRates);
-
-            }
-
-            if (tableType.equalsIgnoreCase("B")) {
-                importedDataB = objectMapper.readValue(responseBody,
-                        new TypeReference<List<TableTypeB>>() {
-                        });
-                //    importedDataB.forEach(System.out::println);
-            }
-
-            if (tableType.equalsIgnoreCase("C")) {
-                importedDataC = objectMapper.readValue(responseBody,
-                        TableTypeC.class);
-                //    String printRates = importedDataC.toString();
-                //    System.out.println(printRates);
-            }
+                TableTypeA importedDataA = null;
+                List<TableTypeB> importedDataB = null;
+                TableTypeC importedDataC = null;
 
 
-            String menu = "Co chcesz zrobić ?:" + "\n" +
-                    "*** jeśli chcesz obliczyć średnią kursu dla podanego przedziału napisz: \"SREDNIA\" " + "\n" +
-                    "*** jeśli chcesz obliczyć odchylenie maxymalne  napisz:  \"ODCHYLENIE\" " + "\n" +
-                    "*** jeśi chcesz obliczyć max i min wartości kursów napisz: \"MINMAX\"  " + "\n" +
-                    "*** jeśli chcesz zakończyć program napisz: \"STOP\"";
+                if (tableType.equalsIgnoreCase("A")) {
+                    importedDataA = objectMapper.readValue(responseBody,
+                            TableTypeA.class);
+                    //     String printRates = importedDataA.toString();
+                    //     System.out.println(printRates);
 
-
-            System.out.println(menu);
-            String komenda = scan.nextLine().toUpperCase();
-            while (!komenda.equalsIgnoreCase("STOP")) {
-
-
-                // ___________________ SREDNIA ______________________________
-
-                if (komenda.equals("SREDNIA") && tableType.equals("A")) {
-                    getAverageForTableA(importedDataA);
-                } else if (komenda.equals("SREDNIA") && tableType.equals("B")) {
-                    System.err.println("brak możliwości policzenia średniej dla tej tabeli");
-                } else if (komenda.equals("SREDNIA") && tableType.equals("C")) {
-                    getAverageForTableC(importedDataC);
                 }
 
-                // ___________________ ODCHYLENIE ______________________________
-
-                if (komenda.equals("ODCHYLENIE") && tableType.equals("A")) {
-                    getDeviationForTableA(importedDataA);
-                } else if (komenda.equals("ODCHYLENIE") && tableType.equals("B")) {
-                    System.err.println("brak możliwości policzenia odchyenia dla tej tabeli");
+                if (tableType.equalsIgnoreCase("B")) {
+                    importedDataB = objectMapper.readValue(responseBody,
+                            new TypeReference<List<TableTypeB>>() {
+                            });
+                    //    importedDataB.forEach(System.out::println);
                 }
-                if (komenda.equals("ODCHYLENIE") && tableType.equals("C")) {
-                    getDeviationForTableC(importedDataC);
+
+                if (tableType.equalsIgnoreCase("C")) {
+                    importedDataC = objectMapper.readValue(responseBody,
+                            TableTypeC.class);
+                    //    String printRates = importedDataC.toString();
+                    //    System.out.println(printRates);
                 }
 
 
-                // ___________________ MIN i MAX ______________________________
+                String menu = "Co chcesz zrobić ?:" + "\n" +
+                        "*** jeśli chcesz obliczyć średnią kursu dla podanego przedziału napisz: \"SREDNIA\" " + "\n" +
+                        "*** jeśli chcesz obliczyć odchylenie maxymalne  napisz:  \"ODCHYLENIE\" " + "\n" +
+                        "*** jeśi chcesz obliczyć max i min wartości kursów napisz: \"MINMAX\"  " + "\n" +
+                        "*** jeśli chcesz zakończyć program napisz: \"STOP\"";
 
-                if (komenda.equals("MINMAX") && tableType.equals("A")) {
-                    getMinMaxForTableA(importedDataA);
-                } else if (komenda.equals("MINMAX") && tableType.equals("B")) {
-                    System.err.println("brak możliwości policzenia min i max dla tej tabeli");
+
+                System.out.println(menu);
+                String komenda = scan.nextLine().toUpperCase();
+                while (!komenda.equalsIgnoreCase("STOP")) {
+
+
+                    // ___________________ SREDNIA ______________________________
+
+                    if (komenda.equals("SREDNIA") && tableType.equals("A")) {
+                        getAverageForTableA(importedDataA);
+                    } else if (komenda.equals("SREDNIA") && tableType.equals("B")) {
+                        System.err.println("brak możliwości policzenia średniej dla tej tabeli");
+                    } else if (komenda.equals("SREDNIA") && tableType.equals("C")) {
+                        getAverageForTableC(importedDataC);
+                    }
+
+                    // ___________________ ODCHYLENIE ______________________________
+
+                    if (komenda.equals("ODCHYLENIE") && tableType.equals("A")) {
+                        getDeviationForTableA(importedDataA);
+                    } else if (komenda.equals("ODCHYLENIE") && tableType.equals("B")) {
+                        System.err.println("brak możliwości policzenia odchyenia dla tej tabeli");
+                    }
+                    if (komenda.equals("ODCHYLENIE") && tableType.equals("C")) {
+                        getDeviationForTableC(importedDataC);
+                    }
+
+                    // ___________________ MIN i MAX ______________________________
+
+                    if (komenda.equals("MINMAX") && tableType.equals("A")) {
+                        getMinMaxForTableA(importedDataA);
+                    } else if (komenda.equals("MINMAX") && tableType.equals("B")) {
+                        System.err.println("brak możliwości policzenia min i max dla tej tabeli");
+                    }
+                    if (komenda.equals("MINMAX") && tableType.equals("C")) {
+                        getMinMaxForTableC(importedDataC);
+                    }
+
+
+                    System.out.println("Podaj nową komendę");
+                    komenda = scan.nextLine().toUpperCase();
                 }
-                if (komenda.equals("MINMAX") && tableType.equals("C")) {
-                    getMinMaxForTableC(importedDataC);
-                }
-
-
-                System.out.println("Podaj nową komendę");
-                komenda = scan.nextLine().toUpperCase();
+            } catch (MismatchedInputException exc) {
+                System.err.println("Błędnie wprowadzony przedział czasowy. Odpal ponownie program i spróbuj z innymi datami. ");
             }
         } catch (NullPointerException | IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+// ________________________________________________METODY_______________________________________________________________
 
-// --------------- metody obiczające SREDNIA w zależności od wybranej tabeli (A/C)-----------------------
+// --------------- metoda obiczająca SREDNIA w zależności od wybranej tabeli (A/C)-----------------------
 
     private static void getAverageForTableA(TableTypeA importedData) {
         OptionalDouble averageA = importedData.getRatesA().stream().mapToDouble(RateA::getMid).average();
@@ -174,7 +195,7 @@ public class Main {
         }
     }
 
-    // -----------------------metody obiczające ODCHYLENIE w zależności od wybranej tabeli (A/C)----------------------
+    // -----------------------metoda obiczająca ODCHYLENIE w zależności od wybranej tabeli (A/C)----------------------
 
     private static void getDeviationForTableA(TableTypeA importedDataA) {
         OptionalDouble max = importedDataA.getRatesA().stream().mapToDouble(RateA::getMid).max();
@@ -205,7 +226,7 @@ public class Main {
     }
 
 
-    // -------------------metody obiczające MIN i MAX w zależności od wybranej tabeli (A/C)----------------
+    // -------------------metoda obiczająca MIN i MAX w zależności od wybranej tabeli (A/C)----------------
 
     private static void getMinMaxForTableA(TableTypeA importedDataA) {
         OptionalDouble max = importedDataA.getRatesA().stream().mapToDouble(RateA::getMid).max();
